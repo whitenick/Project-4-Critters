66cd4673574c9c4744f4edf02127953d456d7141
Nick White <nickwhite@Nick-White-MacBook-Pro.local>
2016-10-05 14:12:23 -0500

    initial

---
+++ Project 4 Critters Proiram/src/assignment4/Header.java
@@ -0,0 +1,13 @@
+package assignment4;
+/* CRITTERS <MyClass.java>
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Nicholas White
+ * Nww295
+ * 16465
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Fall 2016
+ */
